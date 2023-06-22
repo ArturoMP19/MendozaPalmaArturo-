e69d3fe2f8d8f79191450ff79866bf6249f85d7c
ArturoMP19 <a.mendoza.p552@edu.utc.mx>
2023-06-22 08:20:51 -0600

    Sentencias

---
+++ Sentencias.cpp
@@ -0,0 +1,36 @@
+/*
+Ejemplo de sentencias  de control
+*/
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	//Vamos a ver el uso de sentencias 
+	//do while
+	//hacer mientras
+	int i = 1;
+/*	do{
+	//vamos a crear un programa 
+	
+	cout<<"Inmprimir numeros"<<endl;
+	
+	cout<< i <<endl;
+	i++;
+	
+	}while(i<11);*/
+	//While mientras 
+	/*
+	int count =1;
+	while(count<=10){
+		cout<<"Inmprimir numeros"<<endl;
+	
+	cout<< count <<endl;
+	count++;
+	}
+	
+}
+*/
+
+int seleccion;
+
